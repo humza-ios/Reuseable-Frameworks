4968dc61915dcc40db2f3dd840530c8f5216c3d5
humza.shahid <humza.shahid@emaratech.ae>
2017-10-31 10:30:12 +0400

    Firs commit
    Firs commit

---
+++ AOModuleStatus/AOModuleStatus.h
@@ -0,0 +1,19 @@
+//
+//  AOModuleStatus.h
+//  AOModuleStatus
+//
+//  Created by Humza Shahid on 10/31/17.
+//  Copyright © 2017 Humza Shahid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AOModuleStatus.
+FOUNDATION_EXPORT double AOModuleStatusVersionNumber;
+
+//! Project version string for AOModuleStatus.
+FOUNDATION_EXPORT const unsigned char AOModuleStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AOModuleStatus/PublicHeader.h>
+
+
